dc09f8d0212baa4fa5206754e9374c376718e428
Agustina Cortez Marino <tinitamarino@gmail.com>
2022-02-08 20:51:21 -0300

    se agregaron carpetas y archivos

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./views/"));
+// });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/register.html"));
-});
+// app.get("/register", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./views/register.html"));
+// });
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"));
-});
+// app.get("/login", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./views/login.html"));
+// });
