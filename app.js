ff9dde89f2431944c497cba5c83836d839828ff2
Agustina Cortez Marino <tinitamarino@gmail.com>
2022-02-12 03:04:34 -0300

    se creo la vista home con su css

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./views/"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/dinamic/home.html"));
+});
 
 // app.get("/register", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./views/register.html"));
