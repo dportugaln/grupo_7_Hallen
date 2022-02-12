a4a788317b1290a720f527d3d391715392b3e905
Agustina Cortez Marino <tinitamarino@gmail.com>
2022-02-12 16:54:16 -0300

    se agrego chat en home, se conectaron views con botones y se agregaron header y footer en todas las views

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
@@ -13,10 +13,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "src/views/dinamic/home.html"));
 });
 
-// app.get("/register", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./views/register.html"));
-// });
+app.get("/products", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/dinamic/products.html"));
+});
 
-// app.get("/login", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./views/login.html"));
-// });
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/static/contact.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/static/login.html"));
+});
+
+app.get("/aboutus", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/static/aboutus.html"));
+});
